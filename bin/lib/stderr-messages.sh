0e3a4f3855de4bda8509109fe729dd95a5105050
Dan Bornstein <danfuzz@milk.com>
2022-08-05 16:09:27 -0700

    This is no longer needed.

--- bin/lib/stderr-messages.sh
+++ bin/lib/stderr-messages.sh
@@ -82,11 +82,9 @@ function error-msg {
 # Prints a "progress" message to stderr, if such are enabled.
 #
 # --read -- Read messages from stdin.
-# --print-option -- Prints `--progress` or `--no-progress` to stdout, reflecting
-#   the enabled status. (This is to make it easy to propagate the progress state
-#   down into another command.)
 # --set=0|1 -- Enable or disable error message printing.
-# --status -- Prints `1` or `0` to stdout, to indicate enabled status.
+# --status -- Prints `1` or `0` to stdout, to indicate enabled status. (This is
+#   to make it easy to propagate the progress state down into another command.)
 #
 # Note: Progress messages are _disabled by default.
 function progress-msg {
@@ -97,12 +95,6 @@ function progress-msg {
         case "$1" in
             --read)
                 readStdin=1
-                ;;
-            --print-option)
-                (( _stderr_progressEnabled )) \
-                && echo '--progress' \
-                || echo '--no-progress'
-                wasCmd=1
                 ;;
             --set=1|--set=0)
                 _stderr_progressEnabled="${1#*=}"
